5ff79f33e0fb042440648cfefb25cc797c1dfdf3
yahya <yahya682@gmail.com>
2023-07-07 10:34:11 +0700

    update

--- resources/views/pages/dashboard-settings.blade.php
+++ resources/views/pages/dashboard-settings.blade.php
@@ -15,36 +15,42 @@
             <div class="dashboard-content">
                 <div class="row">
                     <div class="col-12">
-                        <form action="">
+                        <form action="{{ route('dashboard-settings-redirect', 'dashboard-settings-store') }}" method="POST"
+                            enctype="multipart/form-data">
+                            @csrf
                             <div class="card">
                                 <div class="card-body">
                                     <div class="row">
                                         <div class="col-md-6">
                                             <div class="form-group">
                                                 <label>Nama Toko</label>
-                                                <input type="text" class="form-control">
+                                                <input type="text" class="form-control" name="store_name"
+                                                    value="{{ $user->store_name }}">
                                             </div>
                                         </div>
                                         <div class="col-md-6">
                                             <div class="form-group">
                                                 <label>Kategori</label>
-                                                <select name="category" id="" class="form-control">
-                                                    <option value="" disabled>Select Category</option>
+                                                <select name="categories_id" class="form-control">
+                                                    <option value="{{ $user->categories_id }}">Tidak diganti</option>
+                                                    @foreach ($categories as $category)
+                                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                                    @endforeach
                                                 </select>
                                             </div>
                                         </div>
                                         <div class="col-md-6">
                                             <div class="form-group">
-                                                <label>Store Status</label>
+                                                <label>Store</label>
                                                 <p class="text-muted">Apakah saat ini toko Anda buka?</p>
                                                 <div class="custom-control custom-radio custom-control-inline">
-                                                    <input type="radio" class="custom-control-input" name="is_store_open"
-                                                        id="openStoreTrue" :value="true">
+                                                    <input type="radio" class="custom-control-input" name="store_status"
+                                                        id="openStoreTrue" value="1" {{ $user->store_status == 1 ? 'checked' : '' }}>
                                                     <label for="openStoreTrue" class="custom-control-label">Buka</label>
                                                 </div>
                                                 <div class="custom-control custom-radio custom-control-inline">
-                                                    <input type="radio" class="custom-control-input" name="is_store_open"
-                                                        id="openStoreFalse" :value="false">
+                                                    <input type="radio" class="custom-control-input" name="store_status"
+                                                        id="openStoreFalse" value="0" {{ $user->store_status == 0 || $user->store_status == NULL ? 'checked' : '' }}>
                                                     <label for="openStoreFalse" class="custom-control-label">Sementara
                                                         Tutup</label>
                                                 </div>
